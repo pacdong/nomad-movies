69c0cb10e84a75738aec13bc17eae1f09b6d2c2d
pacdong <8000pac@naver.com>
2020-03-02 11:55:21 +0900

    2020-03-02 / Custom Icons for the TabNavigation

--- nomard/nomad-movies/navigation/TabNavigation.js
+++ nomard/nomad-movies/navigation/TabNavigation.js
@@ -1,15 +1,48 @@
+import React from "react";
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import MoviesScreen from "../screens/Movies";
 import SearchScreen from "../screens/Search";
 import TVScreen from "../screens/TV";
 import { BG_COLOR } from "../constants/Color";
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator(
   {
-    Movies: MoviesScreen,
-    TV: TVScreen,
-    Search: SearchScreen
+    Movies: {
+      screen: MoviesScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+          />
+        )
+      }
+    },
+    TV: {
+      screen: TVScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+          />
+        )
+      }
+    }
   },
   {
     tabBarOptions: {
