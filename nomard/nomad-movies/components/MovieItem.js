261815489d119504793a5acfa311660f9d12beaf
pacdong <8000pac@naver.com>
2020-03-16 07:31:58 +0900

    2020-03-16 / TV Presenter Complete

--- nomard/nomad-movies/components/MovieItem.js
+++ nomard/nomad-movies/components/MovieItem.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import MoviePoster from "./MoviePoster";
 import MovieRating from "./MovieRating";
+import { GREY_COLOR } from "../constants/Color";
 
 const Container = styled.View`
   align-items: center;
@@ -11,12 +12,50 @@ const Container = styled.View`
 
 const Title = styled.Text`
   color: white;
-  font-size: 12px;
-  margin-vertical: 5px;
+  font-size: ${props => (!props.big ? "12px" : "14px")};
+  margin-top: 5px;
+`;
+const HContainer = styled.View`
+  margin-bottom: 20px;
+  flex-direction: row;
 `;
 
-const MovieItem = ({ id, posterPhoto, title, voteAvg, horizontal = false }) =>
-  horizontal ? null : (
+const Column = styled.View`
+  margin-left: 20px;
+  width: 60%;
+`;
+
+const Overview = styled.Text`
+  color: ${GREY_COLOR};
+  font-size: 12px;
+  margin-top: 10px;
+`;
+
+const MovieItem = ({
+  id,
+  posterPhoto,
+  title,
+  voteAvg,
+  horizontal = false,
+  overview
+}) =>
+  horizontal ? (
+    <HContainer>
+      <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title big={true}>{title}</Title>
+        <MovieRating votes={voteAvg} />
+        {console.log(overview)}
+        {overview ? (
+          <Overview>
+            {overview.length > 150
+              ? `${overview.substring(0, 147)}...`
+              : overview}
+          </Overview>
+        ) : null}
+      </Column>
+    </HContainer>
+  ) : (
     <Container>
       <MoviePoster path={posterPhoto} />
       <Title>
@@ -30,7 +69,8 @@ MovieItem.propTypes = {
   id: PropTypes.number.isRequired,
   posterPhoto: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  voteAvg: PropTypes.number.isRequired
+  voteAvg: PropTypes.number.isRequired,
+  overview: PropTypes.string
 };
 
 export default MovieItem;
