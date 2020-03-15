4802132b90ad5b93632c69fec9d944c26eb84b59
pacdong <8000pac@naver.com>
2020-03-16 08:43:40 +0900

    2020-03-16 / Search Compelete

--- nomard/nomad-movies/screens/Search/SearchContainer.js
+++ nomard/nomad-movies/screens/Search/SearchContainer.js
@@ -1,18 +1,47 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { movies, tv } from "../../api";
 
 export default class extends React.Component {
   state = {
     loading: false,
     movieResults: null,
     tvResults: null,
-    searchTerm: ""
+    searchTerm: "",
+    error: null
   };
 
   handleSearchUpdate = text => {
     this.setState({
       searchTerm: text
     });
+  };
+  onSubmitEditing = async () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      let movieResults, tvResults, error;
+      this.setState({
+        loading: true
+      });
+      try {
+        ({
+          data: { results: movieResults }
+        } = await movies.searchMovies(searchTerm));
+        ({
+          data: { results: tvResults }
+        } = await tv.searchTv(searchTerm));
+      } catch {
+        error = "Can't search";
+      } finally {
+        this.setState({
+          loading: false,
+          movieResults,
+          tvResults,
+          error
+        });
+      }
+      return;
+    }
   };
 
   render() {
@@ -23,6 +52,7 @@ export default class extends React.Component {
         movieResults={movieResults}
         tvResults={tvResults}
         searchTerm={searchTerm}
+        onSubmitEditing={this.onSubmitEditing}
         handleSearchUpdate={this.handleSearchUpdate}
       />
     );
