c60abc90091183e1fabba59c7d43533c99a7dc90
pacdong <8000pac@naver.com>
2020-03-16 08:30:05 +0900

    2020-03-16 / Search CSS and onSubmitEditing

---
+++ nomard/nomad-movies/screens/Search/SearchPresenter.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { BG_COLOR, GREY_COLOR } from "../../constants/Color";
+import Layout from "../../constants/Layout";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${BG_COLOR};
+`;
+
+const InputContainer = styled.View`
+  align-items: center;
+  margin-vertical: 20px;
+`;
+
+const Input = styled.TextInput`
+  background-color: rgba(255, 255, 255, 1);
+  width: ${Layout.width / 1.6};
+  border-radius: 20px;
+  padding: 10px;
+  text-align: center;
+`;
+
+const SearchPresenter = ({
+  loading,
+  tvResults,
+  searchTerm,
+  movieResults,
+  handleSearchUpdate,
+  onSubmitEditing
+}) => (
+  <Container>
+    <InputContainer>
+      <Input
+        onChangeText={handleSearchUpdate}
+        value={searchTerm}
+        returnKeyType="search"
+        placeholder="Search movies and tv"
+        placeholderTextColor={GREY_COLOR}
+        onSubmitEditing={onSubmitEditing}
+      />
+    </InputContainer>
+  </Container>
+);
+
+SearchPresenter.proptypes = {
+  loading: PropTypes.bool.isRequired,
+  tvResults: PropTypes.array,
+  movieResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  handleSearchUpdate: PropTypes.func.isRequired,
+  onSubmitEditing: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
